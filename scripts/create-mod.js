f9917ab2f8a292ccc4805f8d48b787ce6b9b6399
jamestpalmer1979 <jamestpalmer1979@gmail.com>
2015-09-12 23:27:25 +0100

    Started final create-mod script to build mod JSON files

--- scripts/create-mod.js
+++ scripts/create-mod.js
@@ -1,12 +1,64 @@
 var config = require('../config.json');
 var api = require('../src/scripts/api');
-var FileReader = require('filereader');
 var File = require('File');
+var fs = require('fs');
 
-var file = new File('./game-data/json/Tiles.json');
+var io = require('../src/scripts/io');
 
-var callback = function(contents, type, clump) {
-	console.log(arguments);
-};
+var Handlebars = require('handlebars');
 
-api.readFromFile(api.types.Tile, file, callback);
+var templates = loadModTemplates();
+loadGameData(function() {
+	renderTemplates(generateJSON());
+});
+
+function loadGameData(onAllLoaded) {
+	io.resetFilesToLoad();
+	Object.keys(io.fileObjectMap).forEach(function(filename) {
+			var typeName = io.fileObjectMap[filename];
+			var Type = api.types[typeName];
+
+			var filepath = 'game-data/json/'+filename;
+			var file = new File(filepath);
+			console.log('Reading game-data', filepath);
+
+			io.incrementFilesToLoad();
+	    api.readFromFile(Type, file, function(contents, type, clump) {
+	      io.decrementFilesToLoad();
+
+	      if(io.countFilesToLoad() === 0) {
+	      	Object.keys(api.library).forEach(function(typeName) {
+	      		console.log("Loaded", typeName, api.library[typeName].size()+' items');
+	      	});
+	      	console.log('Loading sub-objects and wiring up object references');
+	        api.wireUpObjects();
+	        Object.keys(api.library).forEach(function(typeName) {
+	      		console.log("Discovered", typeName, api.library[typeName].size()+' items');
+	      	});
+
+	      	onAllLoaded();
+	      }
+	    });
+
+	});
+}
+
+function loadModTemplates() {
+	return {
+		events: fs.readFileSync(config.paths.templates+'/events.json.handlebars', { encoding:'utf8' }),
+		qualities: fs.readFileSync(config.paths.templates+'/qualities.json.handlebars', { encoding:'utf8' })
+	};
+}
+
+
+function generateJSON() {
+	return {
+		baseId: api.config.baseId
+	};
+}
+
+function renderTemplates(data) {
+	var qualityTemplate = Handlebars.compile(templates.qualities);
+
+	fs.writeFileSync(config.paths.builddir.mod+'/qualities.json', qualityTemplate(data), { encoding:'utf8' });
+}
